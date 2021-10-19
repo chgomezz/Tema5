c5542220fd8770141e060240b046696de9e104fa
chgomezz <chgomez@fiuna.edu.py>
2021-10-19 15:50:33 -0300

    Create mostrarcamara.py
    Algoritmo que toma los datos de una cámara para luego ser procesado

---
+++ mostrarcamara.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+capturaVideo = cv.VideoCapture(1)
+if not capturaVideo.isOpened():
+    print("No se encontro una camara")
+    exit()
+while True:
+    tipocamara,camara = capturaVideo.read()
+    grises=cv.cvtColor(camara, cv.COLOR_BGR2GRAY)
+
+    cv.imshow("En vivo",grises)
+    if cv.waitKey(1)==ord("q"):
+        break
+capturaVideo.release()
+cv.destroyAllWindows()
